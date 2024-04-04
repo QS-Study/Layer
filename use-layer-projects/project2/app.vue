1b96fea843477cf2b241f6c17cc9ca352565e2ca
Lami <Halam.Lee.Lami@gmail.com>
2024-04-04 11:01:24 +0900

    feat: @qs-study/element-plus-layer를 설치해 element-plus 사용

---
+++ use-layer-projects/project2/app.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="wrapper">
+    <Select />
+    <el-button type="danger">element-plus Button</el-button>
+  </div>
+</template>
+
+<style lang="scss">
+.wrapper {
+  width: 400px;
+  height: 500px;
+  margin: 200px auto 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
